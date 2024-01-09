d190ef31cdb70c3e28d67996b3402a052a25ee95
nebulaw <karlo.tsutskiridze@gmail.com>
2024-01-09 18:01:28 +0400

    initial commit

---
+++ src/test/java/com/nebula/notescape/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebula.notescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
